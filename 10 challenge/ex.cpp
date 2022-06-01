142d45849c93e30fd28f42c39eb86f68ceade7df
Tyrell <tyrellcarter256@gmail.com>
2022-06-01 13:25:01 -0400

    Small change

--- 10 challenge/ex.cpp
+++ 10 challenge/ex.cpp
@@ -4,3 +4,17 @@ using namespace std;
 
 //Will calculate Exponents
 
+void exponents(int base, int power ) {
+    int s = 1;
+    for (int i = 0; i < power; i++)
+    {
+        s *= base;
+    }
+
+    cout << s;
+    
+}
+
+int main() {
+    exponents(2, 8);
+}
